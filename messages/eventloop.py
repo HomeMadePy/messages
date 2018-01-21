859afbbd9dfdeb259301b7930d0e6820b441d905
Timoteo <phillipstr@gmail.com>
2018-01-20 20:33:06 -0500

    adds async message sending, plus required dependencies

---
+++ messages/eventloop.py
@@ -0,0 +1,40 @@
+"""
+Module that will handle asynchronous message sending, so each message will
+be non-blocking.
+"""
+
+from collections import deque
+
+import gevent
+from gevent import monkey
+
+monkey.patch_all()
+
+
+class MessageLoop:
+    """Asynchronous message sending loop."""
+
+    def __init__(self):
+        self.messages = deque()
+        self.loop = None
+
+
+    def add_message(self, message):
+        """add a message to the event loop."""
+        self.messages.append(message)
+        self.send_loop()
+
+
+    def send_loop(self):
+        """start event loop."""
+        #self.loop = asyncio.get_event_loop()
+        tasks = []
+        while self.messages:
+            msg = self.messages.popleft()
+            if hasattr(msg, 'send'):
+                tasks.append(gevent.spawn(msg.send))
+                print('task spawned')
+        print('task complete')
+
+
+MESSAGELOOP = MessageLoop()
--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
     ],
 
     install_requires=[
+        'gevent',
         'twilio',
     ],
 
