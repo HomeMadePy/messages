2f5d18cfc111b1884c7f543918825addeec96e94
Timoteo <phillipstr@gmail.com>
2018-01-20 20:34:54 -0500

    renames email.py to email_.py to avoid conflict with standard library

--- messages/__init__.py
+++ messages/__init__.py
@@ -4,7 +4,7 @@ messages - A package designed to make sending various types of messages easy.
 
 from .chat import WhatsApp
 from .chat import Slack
-from .email import Email
+from ._email import Email
 from .text import Twilio
 
 
