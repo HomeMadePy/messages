4b3391d38997f68da022763e93d5b5c05c6f166e
Timoteo <phillipstr@gmail.com>
2018-02-06 21:56:23 -0500

    continues work on the CLI, but still far from complete. starts adding some tests, as well.

--- messages/cli.py
+++ messages/cli.py
@@ -3,36 +3,65 @@
 import os
 import sys
 
-from .exceptions import MessageTypeError
-from .exceptions import MESSAGES
-
 import click
 from click import option
 
+from .exceptions import MessageTypeError
+from .exceptions import MESSAGES
+from .email_ import Email
+from .text import Twilio
+from .slack import SlackWebhook
+
+
+def _message_factory(msg_type, **kwds):
+    """
+    Factory function that creates the desired message type with the
+    given kwds as arguments.
+    """
+    pass
+
 
 @click.command()
-@click.version_option(version='0.1.2', prog_name='Messages')
-@option('-t', '--type', help='Choose message type.')
-@option('-f', '--from', help='Specify from email/phone/etc.')
+@option('-t', '--type',
+    help='Choose message type.')
+@option('-f', '--from',
+    help='Specify from email/phone/etc.')
 @option('-r', '--recipient', multiple=True,
     help='Specify one or more primary (TO) recipients.')
 @option('-c', '--carboncopy', multiple=True,
     help='Specify one or more carbon copy (CC) addresses.')
 @option('-b', '--blindcopy', multiple=True,
     help='Specify one or more blind carbon copy (BCC) addresses.')
-@option('-s', '--subject', help='Message subject line.')
-@option('-m', '--message', help='Message body -- text or filepath.')
+@option('-s', '--subject',
+    help='Message subject line.')
+@option('-m', '--message',
+    help='Message body -- text or filepath.')
 @option('-a', '--attach', multiple=True,
     help='Specify one or more filepaths or urls to attach.')
+@option('-C', '--configure',
+    help='Configure and preset default values.')
+@click.version_option(version='0.1.2', prog_name='Messages')
 @click.pass_context
 def main(ctx, **kwds):
     """Specify Type, Recipients, and Content to send."""
+
     print(kwds)
 
     if not any([val for key, val in kwds.items()]):
         click.echo(ctx.get_usage())
         click.echo('Try messages --help for more information.')
         sys.exit(0)
+
+    if not kwds['type']:
+        click.echo('** Must specify message type with -t option.')
+        click.echo('Try messages --help for more information.')
+        sys.exit(0)
+
+    if kwds['configure']:
+        #call configuration functions
+        #can be used in tandem with sending an actual message
+        #just save all the values given
+        pass
 
     if kwds['type'].lower() not in MESSAGES:
         raise MessageTypeError(kwds['type'])
@@ -41,3 +70,5 @@ def main(ctx, **kwds):
         if os.path.isfile(kwds['message']):
             message = open(kwds['message'], 'r').read()
 
+    #msg = _message_factory(kwds['type'], kwds)
+    #msg.send_async()
---
+++ tests/test_cli.py
@@ -0,0 +1,57 @@
+"""messages.cli tests."""
+
+import sys
+from unittest.mock import patch
+
+import pytest
+
+import click
+from click.testing import CliRunner
+
+import messages.cli
+from messages.cli import main
+from messages.exceptions import MessageTypeError
+
+
+##############################################################################
+# FIXTURES
+##############################################################################
+
+
+##############################################################################
+# TESTS: cli.main
+##############################################################################
+
+@patch.object(click, 'echo')
+def test_no_args(echo_mock):
+    """
+    GIVEN a call to messages on the CLI
+    WHEN no CLI args are given, i.e. `$ messages `
+    THEN assert sys.exit is called after usage statements are printed
+    """
+    runner = CliRunner()
+    runner.invoke(main, [], catch_exceptions=True)
+    assert echo_mock.call_count == 2
+
+
+@patch.object(click, 'echo')
+def test_no_message_type(echo_mock):
+    """
+    GIVEN a call to messages on the CLI
+    WHEN -t option is not used
+    THEN assert sys.exit is called after usage statements are printed
+    """
+    runner = CliRunner()
+    runner.invoke(main, ['-a', 'file'], catch_exceptions=True)
+    assert echo_mock.call_count == 2
+
+
+def test_unsupported_message_types():
+    """
+    GIVEN a call to messages on the CLI
+    WHEN an unsupported message type is called
+    THEN assert MessageTypeError is raised
+    """
+    runner = CliRunner()
+    with pytest.raises(MessageTypeError):
+        runner.invoke(main, ['-t', 'bad_type'], catch_exceptions=False)
