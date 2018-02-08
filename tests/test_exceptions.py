75f8bf4eedd0accc7a90be65606d26143ccffa22
Timoteo <phillipstr@gmail.com>
2018-02-07 22:03:39 -0500

    adds a test for MessageTypeError

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -3,12 +3,13 @@
 import pytest
 
 from messages.exceptions import MessageInputError
+from messages.exceptions import MessageTypeError
+from messages.exceptions import MESSAGES
 
 
 ##############################################################################
 # TESTS: MessageInputError
 ##############################################################################
-
 
 def test_MessageInputError(capsys):
     """
@@ -22,5 +23,21 @@ def test_MessageInputError(capsys):
         expected = 'Invalid input for specified message class: Email'
         expected += '\n\t* argument: "from_"'
         expected += '\n\t* input type must be: email address\n'
+        assert out == expected
+        assert err == ''
+
+
+def test_MessageTypeError(capsys):
+    """
+    GIVEN no object instantiated, just raise the exception
+    WHEN the exception is raised with given args
+    THEN assert it raises exception and prints proper output
+    """
+    with pytest.raises(MessageTypeError):
+        raise MessageTypeError('BadType')
+        out, err = capsys.readouterr()
+        expected = 'Invalid input for specified message class: BadType'
+        expected += '\n\t* Supported Message Types: '
+        expected += (str(MESSAGES) + '\n')
         assert out == expected
         assert err == ''
