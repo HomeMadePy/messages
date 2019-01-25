d0a527e540a1713477ab3fe6275e72992659c9e4
Timoteo <phillipstr@gmail.com>
2019-01-25 11:28:26 +0300

    adds a setup module for the integration tests

---
+++ tests/integration_tests/int_setup.py
@@ -0,0 +1,13 @@
+"""Common fixtures for integration tests."""
+
+import jsonconfig
+
+def integration_test_configured(msgtype):
+    """
+    Does the user have an 'integration_tester' config profile set up, and
+    do they have 'msgtype' set up in that profile?
+    """
+    with jsonconfig.Config('messages') as cfg:
+        data = cfg.data
+        return ('integration_tester' in cfg.data.keys()
+            and msgtype in data['integration_tester'])
